e3b976d54cfc5438b1cd34cc75b94f5d564081b5
Dmitry Vizersky <dmitry.vizer@gmail.com>
2016-10-25 11:03:02 -0600

    manage window size demo established

--- renderer.js
+++ renderer.js
@@ -4,15 +4,37 @@
 
 console.log("Link established!")
 
-const BrowserWindow = require('electron').remote.BrowserWindow
+const NewBrowserWindow = require('electron').remote.BrowserWindow
 const path = require('path')
 
 const newWindowBtn = document.getElementById('new-window')
 
 newWindowBtn.addEventListener('click', function (event) {
   const modalPath = path.join('file://', __dirname, '../../sections/windows/modal.html')
-  let win = new BrowserWindow({ width: 400, height: 320 })
+  let win = new NewBrowserWindow({ width: 400, height: 320 })
   win.on('close', function () { win = null })
   win.loadURL(modalPath)
   win.show()
+})
+
+
+const ManageBrowserWindow = require('electron').remote.BrowserWindow
+// const path = require('path')
+
+const manageWindowBtn = document.getElementById('manage-window')
+let win
+
+manageWindowBtn.addEventListener('click', function (event) {
+  const modalPath = path.join('file://', __dirname, '../../sections/windows/manage-modal.html')
+  win = new ManageBrowserWindow({ width: 400, height: 275 })
+  win.on('resize', updateReply)
+  win.on('move', updateReply)
+  win.on('close', function () { win = null })
+  win.loadURL(modalPath)
+  win.show()
+  function updateReply () {
+    const manageWindowReply = document.getElementById('manage-window-reply')
+    const message = `Size: ${win.getSize()} Position: ${win.getPosition()}`
+    manageWindowReply.innerText = message
+  }
 })
